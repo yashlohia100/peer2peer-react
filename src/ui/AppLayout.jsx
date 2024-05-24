c88f11f5a272dbed9279a444c8cfc4f6df925f38
Yash <yashlohia105@gmail.com>
2024-05-24 18:13:09 +0530

    Add full width background color

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,8 +4,10 @@ import Footer from './Footer';
 
 export default function AppLayout() {
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      <Navbar />
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-zinc-100 text-red-500">
+      <div className="bg-emerald-500">
+        <Navbar />
+      </div>
 
       <div>
         <main className="mx-auto max-w-6xl">
