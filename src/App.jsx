f861da1e1ac19eec26fa647ef6d35c485745ae15
Yash <yashlohia105@gmail.com>
2024-05-24 08:01:32 +0530

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './ui/Homepage';
+import ListingsPage from './features/listing/ListingsPage';
+import ListingDetail from './features/listing/ListingDetail';
+import Login from './features/user/Login';
+import Signup from './features/user/Signup';
+import PageNotFound from './ui/PageNotFound';
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold text-red-500">Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/listings" element={<ListingsPage />} />
+        <Route path="/listings/:listingId" element={<ListingDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
