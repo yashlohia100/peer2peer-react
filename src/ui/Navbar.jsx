1769f02e3efc738513fabe0c8e2f31df5ba2fa45
Yash <yashlohia105@gmail.com>
2024-05-24 18:10:51 +0530

    Create 'Navbar' component

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-  return <nav className="mx-auto w-full max-w-6xl">Navbar</nav>;
+  return (
+    <nav className="mx-auto flex max-w-6xl items-center justify-between bg-emerald-500 px-4 py-2 text-emerald-100">
+      <h1 className="text-2xl font-medium">Peer2peer</h1>
+
+      <div className="space-x-3 text-sm font-semibold uppercase tracking-wide md:space-x-4">
+        <Link to="login">Login</Link>
+        <Link to="signup">Signup</Link>
+      </div>
+    </nav>
+  );
 }
