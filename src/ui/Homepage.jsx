1d0d1bf148661afaa46d1c8335985e02419bcc47
Yash <yashlohia105@gmail.com>
2024-05-24 18:17:09 +0530

    Create 'Homepage'

--- src/ui/Homepage.jsx
+++ src/ui/Homepage.jsx
@@ -1,3 +1,17 @@
+import { HiOutlineHome, HiOutlineUsers } from 'react-icons/hi2';
+import IconBox from './IconBox';
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className="px-4 py-2">
+      <h2 className="mt-12 text-center text-2xl font-medium text-zinc-600">
+        What are you looking for?
+      </h2>
+
+      <div className="mt-10 flex items-center justify-center gap-6 pb-8 md:gap-8">
+        <IconBox icon={<HiOutlineHome />} text="Room" />
+        <IconBox icon={<HiOutlineUsers />} text="Roommate" />
+      </div>
+    </div>
+  );
 }
