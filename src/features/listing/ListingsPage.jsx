f2813ad4f7c2cd7103e1a4a63c73e33eb62d4ab4
Yash <yashlohia105@gmail.com>
2024-05-25 20:28:26 +0530

    Add 'SelectLocation' and 'ListingNavbar'

--- src/features/listing/ListingsPage.jsx
+++ src/features/listing/ListingsPage.jsx
@@ -1,3 +1,13 @@
+import SelectLocation from '../../ui/SelectLocation';
+import ListingNavbar from './ListingNavbar';
+
 export default function ListingsPage() {
-  return <div>ListingsPage</div>;
+  return (
+    <div className="px-2 pt-4">
+      <div className="flex flex-col gap-6 border-b border-gray-300 pb-1 lg:flex-row-reverse lg:justify-between lg:pb-2 lg:pt-6">
+        <SelectLocation />
+        <ListingNavbar />
+      </div>
+    </div>
+  );
 }
