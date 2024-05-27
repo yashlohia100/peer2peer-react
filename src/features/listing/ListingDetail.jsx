aa4ec3779dc5a2c57828f8ae34aa1abc6991dbc1
Yash <yashlohia105@gmail.com>
2024-05-27 06:22:15 +0530

    Add divider

--- src/features/listing/ListingDetail.jsx
+++ src/features/listing/ListingDetail.jsx
@@ -1,9 +1,11 @@
+import BasicInfo from './listing-ui/BasicInfo';
 import PersonInfo from './listing-ui/PersonInfo';
 
 export default function ListingDetail() {
   return (
-    <div>
+    <div className="divide-y-2 divide-gray-300 p-4">
       <PersonInfo />
+      <BasicInfo />
     </div>
   );
 }
