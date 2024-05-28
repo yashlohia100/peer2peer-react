20eaad15e1fd667842c09a8ed8bc6c6067649bae
Yash <yashlohia105@gmail.com>
2024-05-29 02:12:03 +0530

    Create facilities section

--- src/features/listing/ListingDetail.jsx
+++ src/features/listing/ListingDetail.jsx
@@ -1,11 +1,15 @@
 import BasicInfo from './listing-ui/BasicInfo';
+import Facilities from './listing-ui/Facilities';
 import PersonInfo from './listing-ui/PersonInfo';
+import Preferences from './listing-ui/Preferences';
 
 export default function ListingDetail() {
   return (
     <div className="divide-y-2 divide-gray-300 p-4">
       <PersonInfo />
       <BasicInfo />
+      <Preferences />
+      <Facilities />
     </div>
   );
 }
