477f91e967e4aafdac47397f473a6e1406a4b085
Yash <yashlohia105@gmail.com>
2024-06-15 06:53:25 +0530

    Create footer

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 export default function AppLayout() {
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-zinc-100 text-red-500">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-zinc-50 text-red-500">
       <div className="bg-emerald-500">
         <Navbar />
       </div>
@@ -13,9 +13,9 @@ export default function AppLayout() {
         <main className="mx-auto max-w-6xl">
           <Outlet />
         </main>
-      </div>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer className="mx-auto w-full max-w-6xl">Footer</footer>;
+  return (
+    <footer className="mx-auto mt-4 flex max-w-6xl items-center justify-between border-t-2 px-4 py-5">
+      <p className="text-xl font-medium text-emerald-400">Peer2peer</p>
+
+      <p className="text-sm text-zinc-400">&copy; Yash Lohia</p>
+    </footer>
+  );
 }
