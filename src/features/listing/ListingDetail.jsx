178466f53a8b5bee417dcd452f327e19245b1bd2
Yash <yashlohia105@gmail.com>
2024-06-15 06:13:37 +0530

    Display two columns on large screens

--- src/features/listing/ListingDetail.jsx
+++ src/features/listing/ListingDetail.jsx
@@ -7,13 +7,18 @@ import Preferences from './listing-ui/Preferences';
 
 export default function ListingDetail() {
   return (
-    <div className="divide-y-2 divide-gray-300 p-4">
-      <PersonInfo />
-      <BasicInfo />
-      <Carousel />
-      <Preferences />
-      <Facilities />
-      <ExtraInfo />
+    <div className="p-4 lg:grid lg:grid-cols-[1fr_2fr]">
+      <div className="border-b-2 border-gray-300 lg:mt-20 lg:border-none">
+        <PersonInfo />
+      </div>
+
+      <div className="divide-y-2 divide-gray-300">
+        <BasicInfo />
+        <Carousel />
+        <Preferences />
+        <Facilities />
+        <ExtraInfo />
+      </div>
     </div>
   );
 }
