f35cdddcc52c5d6ac23d92f5ecd58ca78c964981
Yash <yashlohia105@gmail.com>
2024-06-27 21:29:51 +0530

    Filter by listing type

--- src/features/listing/useGetListings.js
+++ src/features/listing/useGetListings.js
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAllListingsApi } from '../../services/apiFunctions';
+import { useSearchParams } from 'react-router-dom';
 
 export function useGetListings() {
+  const [searchParams] = useSearchParams();
+  const type = searchParams.get('type');
+
   const {
     isPending,
     data: listings,
     error,
   } = useQuery({
     queryKey: ['listings'],
-    queryFn: getAllListingsApi,
+    queryFn: () => getAllListingsApi(type),
   });
 
   return { isPending, listings, error };
--- src/services/apiFunctions.js
+++ src/services/apiFunctions.js
@@ -1,5 +1,12 @@
-export async function getAllListingsApi() {
-  const res = await fetch(`http://localhost:8000/api/listings`);
+export async function getAllListingsApi(type) {
+  let url = null;
+  if (type) {
+    url = `http://localhost:8000/api/listings?listingType=${type}`;
+  } else {
+    url = `http://localhost:8000/api/listings`;
+  }
+
+  const res = await fetch(url);
   if (!res.ok) throw new Error('Unable to get listings.');
   const data = await res.json();
   return data.listings;
