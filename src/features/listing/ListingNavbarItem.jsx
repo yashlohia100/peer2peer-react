64523d26cd7f2fd54baa5cc1a5048fd950954ff0
Yash <yashlohia105@gmail.com>
2024-06-28 19:16:59 +0530

    Preserve city param

--- src/features/listing/ListingNavbarItem.jsx
+++ src/features/listing/ListingNavbarItem.jsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function ListingNavbarItem({ icon, text, toListingType }) {
+  const [searchParams] = useSearchParams();
+  const city = searchParams.get('city');
+
   return (
     <Link
-      to={`?type=${toListingType}`}
+      to={`?type=${toListingType}&city=${city}`}
       className="flex flex-col items-center gap-1 border-b-2 border-transparent px-2 py-1 text-zinc-500 transition duration-300 hover:text-zinc-700 focus:border-zinc-600 focus:text-zinc-700 focus:outline-none"
     >
       <span className="text-2xl">{icon}</span>
