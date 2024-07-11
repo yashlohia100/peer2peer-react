9bbd6106ee53b489f977efca7c53ba40dbd3edf3
Yash <yashlohia105@gmail.com>
2024-07-11 06:25:03 +0530

    Render images in checkbox

--- src/ui/form-ui/CheckboxAsImage.jsx
+++ src/ui/form-ui/CheckboxAsImage.jsx
@@ -1,4 +1,8 @@
 export default function CheckboxAsImage({ text, register, varName }) {
+  const imageName = text.toLowerCase().replaceAll(' ', '-') + '.png';
+  const imageUrl =
+    import.meta.env.VITE_IMAGE_URL + '/' + varName + '/' + imageName;
+
   return (
     <li>
       <input
@@ -14,7 +18,7 @@ export default function CheckboxAsImage({ text, register, varName }) {
         htmlFor={text}
         className="flex flex-col items-center gap-1 rounded-md bg-zinc-100 p-2 text-zinc-800 transition-colors hover:cursor-pointer peer-checked:bg-emerald-500 peer-checked:text-emerald-50"
       >
-        <img src="/night-owl.png" alt={text} className="h-16 w-16" />
+        <img src={imageUrl} alt={text} className="h-16 w-16 object-contain" />
         <span className="text-xs font-medium capitalize md:text-sm">
           {text}
         </span>
