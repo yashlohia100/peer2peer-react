ce374460ed5308b27dde6e8b07c2017f78cada26
Yash <yashlohia105@gmail.com>
2024-09-18 08:48:21 +0530

    Redirect to listing page on success

--- src/features/listing/useCreateListing.js
+++ src/features/listing/useCreateListing.js
@@ -1,12 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
 import { createListingApi } from '../../services/apiFunctions';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export function useCreateListing() {
+  const navigate = useNavigate();
+
   const { isPending, mutate: createListing } = useMutation({
     mutationFn: createListingApi,
-    onSuccess: () => {
+    onSuccess: (listing) => {
       toast.success('Listing created successfully.');
+
+      setTimeout(() => {
+        navigate(`/listings/${listing._id}`);
+      }, 1000);
     },
     onError: () => {
       toast.error('Error creating listing.');
