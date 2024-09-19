4bf306a9415f2986ced3b2197c942969b5f22cf9
Yash <yashlohia105@gmail.com>
2024-09-20 01:38:55 +0530

    Add select state to homepage buttons

--- src/ui/Homepage.jsx
+++ src/ui/Homepage.jsx
@@ -1,8 +1,22 @@
 import { HiOutlineHome, HiOutlineUsers } from 'react-icons/hi2';
 import IconBox from './IconBox';
 import SelectLocation from './SelectLocation';
+import { useState } from 'react';
 
 export default function Homepage() {
+  const [listingType, setListingType] = useState('room');
+
+  const isRoomSelected = listingType === 'room';
+  const isRoommateSelected = listingType === 'roommate';
+
+  function handleSetListingTypeToRoom() {
+    setListingType('room');
+  }
+
+  function handleSetListingTypeToRoommate() {
+    setListingType('roommate');
+  }
+
   return (
     <div className="min-h-screen px-4 py-2">
       <h2 className="mt-12 text-center text-2xl font-medium text-zinc-600">
@@ -10,8 +24,18 @@ export default function Homepage() {
       </h2>
 
       <div className="mt-10 flex items-center justify-center gap-6 pb-8 md:gap-8">
-        <IconBox icon={<HiOutlineHome />} text="Room" />
-        <IconBox icon={<HiOutlineUsers />} text="Roommate" />
+        <IconBox
+          icon={<HiOutlineHome />}
+          text="Room"
+          isSelected={isRoomSelected}
+          onClick={handleSetListingTypeToRoom}
+        />
+        <IconBox
+          icon={<HiOutlineUsers />}
+          text="Roommate"
+          isSelected={isRoommateSelected}
+          onClick={handleSetListingTypeToRoommate}
+        />
       </div>
 
       <SelectLocation />
--- src/ui/IconBox.jsx
+++ src/ui/IconBox.jsx
@@ -1,7 +1,8 @@
-export default function IconBox({ icon, text, isSelected }) {
+export default function IconBox({ icon, text, isSelected, onClick }) {
   return (
     <button
-      className={`rounded-lg border border-zinc-400 px-4 py-2 text-zinc-400 transition duration-300 hover:bg-zinc-200/30 hover:text-emerald-500 hover:shadow-lg ${isSelected ? 'bg-zinc-200/30 text-emerald-500 shadow-lg' : ''}`}
+      className={`rounded-lg border border-zinc-400 px-4 py-2 transition duration-300 hover:bg-zinc-200/30 hover:text-emerald-500 hover:shadow-lg ${isSelected ? 'bg-zinc-200/30 text-emerald-500 shadow-lg' : 'text-zinc-400'}`}
+      onClick={onClick}
     >
       <span className="text-8xl">{icon}</span>
 
